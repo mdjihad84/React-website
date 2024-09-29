add81c5a8406025b368078deb18f0b4329df1ce7
MD JAHIDUL ISLAM JIHAD <mdj847621@gmail.com>
2024-09-29 23:25:13 +0600

    folder setup complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>bangladesh was small country</h1>
+    </div>
+  );
+};
+
+export default App;
